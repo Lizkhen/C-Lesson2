1df8c09d301585e4d0b0e17d808a802da4299fa6
Lizkhen <slezadrakona@gmail.com>
2022-12-04 19:16:44 +0300

    Task 15, 16

---
+++ Task15/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] array1 = {1,2,3,4,5};
+int [] array2 = {6,7};
+foreach (int i in array1)
+{
+    if (i == number)
+    {
+        Console.WriteLine("Рабочий");
+    }
+}
+foreach (int a in array2)
+{
+    if (a == number)
+    {
+        Console.WriteLine("Выходной");
+    }
+}
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Такого дня недели не существует!!!");
+}
+
