bd8792299924ec870b0ff0ab9d165411d2701893
Lizkhen <slezadrakona@gmail.com>
2022-12-04 18:59:32 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = (number % 100) / 10;
+if (a < 0)
+{
+    a = -a;
+}
+if (number/100 < 1||number/100 > 10)
+{
+    Console.WriteLine("Нет-нет-нет! Только ТРЁХЗНАЧНОЕ число!!!");
+}
+else 
+    Console.WriteLine ("Вторая цифра " + a);
